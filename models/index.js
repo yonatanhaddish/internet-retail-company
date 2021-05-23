bc8866da2700a745538ee6267dd876d761bde4cc
Your Name <you@example.com>
2021-05-23 13:45:38 -0400

    model created

--- models/index.js
+++ models/index.js
@@ -4,13 +4,48 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
 // Categories have many Products
-
+// Products belongsTo Category
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'aaa',
+  foreignKey: 'product_id'
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'aaa',
+  foreignKey: 'tag_id'
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id'
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id'
+});
+
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
