6d68af5911c59a6b7b6526bfca254ff8f1ec2da8
Your Name <you@example.com>
2021-05-23 16:21:03 -0400

    categoreis routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,7 +23,13 @@ router.get('/:id', (req, res) => {
   Category.findOne({
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
   })
   .then(dbCategoryData => {
     if (!dbCategoryData) {
